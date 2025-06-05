51298708246ff9f94c5c01b88c9686eade44d614
kingpoker80 <54614601+kingpoker80@users.noreply.github.com>
2025-06-05 16:18:43 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import New from "./pages/New";
@@ -6,35 +6,15 @@ import Diary from "./pages/Diary";
 import Edit from "./pages/Edit";
 import React, { useReducer, useRef, useEffect, useState } from "react";
 
-// import {getEmotionImgById} from "./util";
+// Context 생성
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
 
-// function reducer(state, action) {
-//   switch (action.type) {
-//     case "INIT": {
-//       return action.data;
-//     }
-//     case "CREATE": {
-//       return [action.data, ...state];
-//     }
-//     case "UPDATE": {
-//       return state.map((it) =>
-//         String(it.id) === String(action.data.id) ? { ...action.data } : it
-//       );
-//     }
-//     case "DELETE": {
-//       return state.filter((it) => String(it.id) !== String(action.targetId));
-//     }
-//     default: {
-//       return false;
-//     }
-//   }
-// }
-//로컬 스토로지 사용.
+// Reducer
 function reducer(state, action) {
   switch (action.type) {
-    case "INIT": {
+    case "INIT":
       return action.data;
-    }
     case "CREATE": {
       const newState = [action.data, ...state];
       localStorage.setItem("diary", JSON.stringify(newState));
@@ -54,37 +34,10 @@ function reducer(state, action) {
       localStorage.setItem("diary", JSON.stringify(newState));
       return newState;
     }
-
-    default: {
+    default:
       return state;
-    }
   }
 }
-
-const mockData = [
-  {
-    id: "mock1",
-    date: new Date().getTime() - 1,
-    content: "mock1",
-    emotionId: 1,
-  },
-  {
-    id: "mock2",
-    date: new Date().getTime() - 22,
-    content: "mock2",
-    emotionId: 2,
-  },
-  {
-    id: "mock3",
-    date: new Date().getTime() - 33,
-    content: "mock3",
-    emotionId: 3,
-  },
-];
-
-//props drilling 없게 하기 위해.
-export const DiaryStateContext = React.createContext();
-export const DiaryDispatchContext = React.createContext();
 
 function App() {
   const [isDataLoaded, setIsDataLoader] = useState(false);
@@ -92,11 +45,6 @@ function App() {
   const [data, dispatch] = useReducer(reducer, []);
 
   useEffect(() => {
-    // dispatch({
-    //   type: "INIT",
-    //   data: mockData,
-    // });
-    // setIsDataLoader(true);
     const rawData = localStorage.getItem("diary");
     if (!rawData) {
       setIsDataLoader(true);
@@ -107,9 +55,8 @@ function App() {
       setIsDataLoader(true);
       return;
     }
-    //MAX ID 채번.
     localData.sort((a, b) => Number(b.id) - Number(a.id));
-    idRef.current = localData[0].id + 1;
+    idRef.current = Number(localData[0].id) + 1;
 
     dispatch({ type: "INIT", data: localData });
     setIsDataLoader(true);
@@ -127,6 +74,7 @@ function App() {
     });
     idRef.current += 1;
   };
+
   const onUpdate = (targetId, date, content, emotionId) => {
     dispatch({
       type: "UPDATE",
@@ -138,6 +86,7 @@ function App() {
       },
     });
   };
+
   const onDelete = (targetId) => {
     dispatch({
       type: "DELETE",
@@ -147,36 +96,26 @@ function App() {
 
   if (!isDataLoaded) {
     return <div>데이터 로딩중.</div>;
-  } else {
-    return (
-      <DiaryStateContext.Provider value={data}>
-        <DiaryDispatchContext.Provider
-          value={{
-            onCreate,
-            onUpdate,
-            onDelete,
-          }}
-        >
-          <div className="App">
-            {/* <h1>감정 일기장</h1> */}
-            {/* <img alt="감정1" src={getEmotionImgById(2)} /> */}
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/new" element={<New />} />
-              <Route path="/diary/:id" element={<Diary />} />
-              <Route path="/edit/:id" element={<Edit />} />
-            </Routes>
-            {/* <div>
-              <Link to={"/"}>Home</Link>
-              <Link to={"/new"}>New</Link>
-              <Link to={"/diary"}>Diary</Link>
-              <Link to={"/edit"}>Edit</Link>
-            </div> */}
-          </div>
-        </DiaryDispatchContext.Provider>
-      </DiaryStateContext.Provider>
-    );
   }
+
+  // 개발 환경이면 BrowserRouter, 배포 환경이면 HashRouter 사용
+  const isLocal = window.location.hostname === "localhost";
+  const Router = isLocal ? BrowserRouter : HashRouter;
+
+  return (
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider value={{ onCreate, onUpdate, onDelete }}>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/new" element={<New />} />
+            <Route path="/diary/:id" element={<Diary />} />
+            <Route path="/edit/:id" element={<Edit />} />
+          </Routes>
+        </div>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
+  );
 }
 
 export default App;
